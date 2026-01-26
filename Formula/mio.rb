60e551c012d22e63da5211e740ffb1c654dbca9e
Thies <thies@example.com>
2026-01-26 11:59:09 +0100

    Fix mio 0.3.0 formula: use correct tar.gz asset URLs and checksums

--- Formula/mio.rb
+++ Formula/mio.rb
@@ -6,40 +6,28 @@ class Mio < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio-darwin-arm64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Darwin_arm64.tar.gz"
+      sha256 "2153e946cd6752ecaa92182f66d27f0c69ac98e9648d581d212226a9b2a74ce4"
     end
     on_intel do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio-darwin-amd64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Darwin_x86_64.tar.gz"
+      sha256 "d4cf2fc60209302af8ab5933bbbee898a7599108235c12aad1c884174ab4626a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio-linux-arm64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Linux_arm64.tar.gz"
+      sha256 "cfd2daf1b372f441ad41939deb09192b638f63394c6b6a421444d7dfb119e578"
     end
     on_intel do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio-linux-amd64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Linux_x86_64.tar.gz"
+      sha256 "212d60b6927c422d633f98c6015b07873a35eea705ae41b5bd84e607d0bad190"
     end
   end
 
   def install
-    if OS.mac?
-      if Hardware::CPU.arm?
-        bin.install "mio-darwin-arm64" => "mio"
-      else
-        bin.install "mio-darwin-amd64" => "mio"
-      end
-    elsif OS.linux?
-      if Hardware::CPU.arm?
-        bin.install "mio-linux-arm64" => "mio"
-      else
-        bin.install "mio-linux-amd64" => "mio"
-      end
-    end
+    bin.install "mio"
   end
 
   test do
