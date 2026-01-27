9a4e94b43000170e672a146ef1a3ad6686afc316
Thies <thies@example.com>
2026-01-27 13:18:25 +0100

    Update mio to v0.4.0

--- Formula/mio.rb
+++ Formula/mio.rb
@@ -1,28 +1,28 @@
 class Mio < Formula
   desc "MySQL CLI with multi-server support and advanced table discovery"
   homepage "https://github.com/thieso2/mio"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Darwin_arm64.tar.gz"
-      sha256 "2153e946cd6752ecaa92182f66d27f0c69ac98e9648d581d212226a9b2a74ce4"
+      url "https://github.com/thieso2/mio/releases/download/v0.4.0/mio_0.4.0_Darwin_arm64.tar.gz"
+      sha256 "99f046cdf7d5d29cc8b356df22c05f47aa4a2610762c5a89b02502f2b4b2db0b"
     end
     on_intel do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Darwin_x86_64.tar.gz"
-      sha256 "d4cf2fc60209302af8ab5933bbbee898a7599108235c12aad1c884174ab4626a"
+      url "https://github.com/thieso2/mio/releases/download/v0.4.0/mio_0.4.0_Darwin_x86_64.tar.gz"
+      sha256 "8a9293a5902f6a576afbb5cd0e6835f61a616ff75cb754185844003bc6ab8262"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Linux_arm64.tar.gz"
-      sha256 "cfd2daf1b372f441ad41939deb09192b638f63394c6b6a421444d7dfb119e578"
+      url "https://github.com/thieso2/mio/releases/download/v0.4.0/mio_0.4.0_Linux_arm64.tar.gz"
+      sha256 "21ea6dc3292ccfb7114125c91bfb255acc5b557b730e398b5a6d5cd15192800d"
     end
     on_intel do
-      url "https://github.com/thieso2/mio/releases/download/v0.3.0/mio_0.3.0_Linux_x86_64.tar.gz"
-      sha256 "212d60b6927c422d633f98c6015b07873a35eea705ae41b5bd84e607d0bad190"
+      url "https://github.com/thieso2/mio/releases/download/v0.4.0/mio_0.4.0_Linux_x86_64.tar.gz"
+      sha256 "f1305d41ab8b94ebb7580f84f3546f70f63a2965661bf1d95724390af9bc2e95"
     end
   end
 
